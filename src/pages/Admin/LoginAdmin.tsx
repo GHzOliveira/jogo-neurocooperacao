9f20845a4cc5d6a5caefc245e2f6881822e97729
Guilherme Oliveira <72478821+GHzOliveira@users.noreply.github.com>
2024-05-01 09:25:12 -0300

    chore: testando rota

--- src/pages/Admin/LoginAdmin.tsx
+++ src/pages/Admin/LoginAdmin.tsx
@@ -15,7 +15,7 @@ export default function LoginAdmin() {
     const onSubmit: SubmitHandler<IFormInput> = async (data) => {
         try {
             const response = await axios.get(
-                `http://localhost:3333/admin/login?user=${data.user}&password=${data.password}`,
+                `https://neurocooperacao-backend.vercel.app/admin/login?user=${data.user}&password=${data.password}`,
             );
             if (response.data.status === 200) {
                 navigate('/admin/painel-controll');
